ed6465e8712e84c7841b3f34f4e09e10cb1bdabd
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-09 19:01:52 -0400

    better react setup script

--- zsh/oh-my-zsh/custom/react-scripts/reactsetup.zsh
+++ zsh/oh-my-zsh/custom/react-scripts/reactsetup.zsh
@@ -1,19 +1,24 @@
 reactsetup() {
-	npm i customize-cra react-app-rewired styled-components babel-plugin-styled-components
+	npm i customize-cra react-app-rewired styled-components babel-plugin-styled-components react-gallery-carousel react-router-dom
 	git clone git@github.com:Gittenger/react-setup.git
 	
 	## VSCODE SETTINGS/GITIGNORE
-	cp -r ./react-setup/.vscode/. ./react-setup/config-overrides.js ./react-setup/.gitignore ./
+	cd react-setup
+	cp -r .vscode .env config-overrides.js .gitignore ../
+	cd ../
 	
 	## REMOVE UNNEEDED FILES
-	yes | rm ./src/* 
-	rm ./src/.*
-
+	setopt localoptions rmstarsilent
+	yes | rm -f src/* 
+	yes | rm -f src/.* 
+	
 	## COPY SRC
 	cp -r ./react-setup/src/. ./src
 
 	## REMOVE CLONE
 	rm -rf ./react-setup
+
+	unsetopt localoptions rmstarsilent
 
 	## SCRIPT TO UPDATE PACKAGE.JSON
 	node $NODE_SCRIPTS/updatePackageForRAR.js package.json
