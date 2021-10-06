3eeee17de4a70506f714fa5bc1e7e6bc23c18b52
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-10-06 00:18:21 -0400

    fzdir updated to fzd

--- zsh/oh-my-zsh/custom/react-scripts/rxcomp.zsh
+++ zsh/oh-my-zsh/custom/react-scripts/rxcomp.zsh
@@ -1,7 +1,7 @@
 rxcomp() {
    componentLib=$REACT_COMPONENT_LIB
 
-   results=($(fzdir -m))
+   results=($(fzd -m))
    [[ -n "$results" ]] && cp -r ${results[*]} $componentLib
 
    unset componentLib results
