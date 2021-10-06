3d8f2a51aa5be6fc286ebc15241d031d6d99a062
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-10-06 00:17:08 -0400

    imports working correctly with categories

--- zsh/oh-my-zsh/custom/react-scripts/ricomp.zsh
+++ zsh/oh-my-zsh/custom/react-scripts/ricomp.zsh
@@ -1,6 +1,19 @@
 ricomp() {
+   # import directly into category folder, ie: Admin, General, etc
    componentLib=$REACT_COMPONENT_LIB
    results=($(fd -t d . ${componentLib} | fzf -m))
+   cwd=$(pwd)
+   [[ -n "$results" ]] && cp -r ${results[*]} $cwd
+
+	node $NODE_SCRIPTS/updateComp.script.js ../
+
+   unset componentLib results cwd
+}
+
+ricompcat() {
+   # import directly into components folder
+   componentLib=$REACT_COMPONENT_LIB
+   results=($(fd -t d -d 1 . ${componentLib} | fzf -m))
    cwd=$(pwd)
    [[ -n "$results" ]] && cp -r ${results[*]} $cwd
 
@@ -8,3 +21,4 @@ ricomp() {
 
    unset componentLib results cwd
 }
+
