6d1ab82dd25aeda4ab6adefd41d00ec75b7591cd
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-10-21 14:16:26 -0400

    script to fix prev renamed album names

---
+++ scripts/node/fixRenameMusic.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const fs = require('fs')
+
+// path variables for use below
+const albumStr = process.argv[2]
+const albumAbs = path.resolve(albumStr)
+
+// check for year
+const yearRegex = /((19|20)\d\d)/
+let year = ''
+let resultStr = ''
+if (yearRegex.test(albumStr)) {
+	const match = albumStr.match(yearRegex)
+	year += match[1]
+	// remove year from string
+	resultStr = albumStr.replace(yearRegex, '')
+} else resultStr += albumStr
+
+// remove []
+resultStr = resultStr.replace(/[\[\]\|]/g, ' ').trim()
+
+let final = ''
+if (year) {
+	final = String.prototype.concat('(', year, ') | ', resultStr)
+} else {
+	final = resultStr
+}
+
+fs.renameSync(path.resolve(albumAbs), final)
+
+// final consideration is to remove adds for blogs, although this is only a minor concern as it's not too common
