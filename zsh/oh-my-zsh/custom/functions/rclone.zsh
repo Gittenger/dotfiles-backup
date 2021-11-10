bbd374e20e1ead8af5c21c66a420c0c8968a944d
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-11-10 13:44:48 -0500

    rclcp rclls, rclall

--- zsh/oh-my-zsh/custom/functions/rclone.zsh
+++ zsh/oh-my-zsh/custom/functions/rclone.zsh
@@ -1,10 +1,14 @@
-rclone() {
-	if [[ "$#" -eq "0" ]]
-		then
-			/usr/local/rclone/rclone --help
-		else
-			/usr/local/rclone/rclone "$@"
-	fi		
+# rclone() {
+# 	if [[ "$#" -eq "0" ]]
+# 		then
+# 			/usr/local/rclone/rclone --help
+# 		else
+# 			/usr/local/rclone/rclone "$@"
+# 	fi		
+# }
+
+rclls() {
+	rclone ls remote:synced-folders/home
 }
 
 rclcp() {
@@ -13,16 +17,14 @@ rclcp() {
 			echo "Error: expected rclone args. See rclone --help"
 			exit 1
 		else
-			rclone copy $1 driveFolders:synced-folders/$2 
+			rclone copy $1 remote:synced-folders/home/$2 
 	fi		
 }
 
 rclall() {
-   rclcp $HOME/Documents/0-learning/practical-bash-scripting "practical-bash-scripting"
-   rclcp $HOME/Documents/0-learning/vim-notes "vim-notes"
-   rclcp $HOME/Documents/0-learning/design-patterns-notes "design-patterns-notes"
-   rclcp $HOME/Documents/0-learning/textbook-notes/ "textbook-notes"
-   rclcp $HOME/Documents/my-linux-notes "my-linux-notes"
-   rclcp $HOME/Documents/misc/blog-posts "blog-posts"
-   rclcp $HOME/assets/my-assets/icons/svg "svg-icons"
+   rclcp $RCLONE_NOTES "notes"
+   rclcp $RCLONE_BOOKS "books"
+   rclcp $RCLONE_LANG "languages"
+   rclcp $RCLONE_BLOG "blog-posts"
+   rclcp $RCLONE_GNOTE "gnote"
 }
